bf779799cb25ff015a8ec0f474b789343e6e3d6d
Manuel Aguado <manuelaguado@gmail.com>
2018-09-20 01:36:53 -0500

    eliminar comentarios sin terminar

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -14,17 +14,3 @@ class EncryptCookies extends Middleware
         //
     ];
 }
-
-
-
-/*
-namespace App\Http\Middleware;
-
-use Illuminate\Cookie\Middleware\EncryptCookies as BaseEncrypter;
-
-class EncryptCookies extends BaseEncrypter
-{
-    protected $except = [
-
-    ];
-}
